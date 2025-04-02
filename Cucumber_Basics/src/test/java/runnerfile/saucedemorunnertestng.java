85e434b507f4534a30734066a959da83542b417e
Divraj-M <2K21cse022@kiot.ac.in>
2025-04-02 15:54:10 +0530

    updated the all the base file

--- Cucumber_Basics/src/test/java/runnerfile/saucedemorunnertestng.java
+++ Cucumber_Basics/src/test/java/runnerfile/saucedemorunnertestng.java
@@ -1,11 +1,14 @@
-package runnerfile;
-import io.cucumber.testng.CucumberOptions;
-import io.cucumber.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(plugin={"pretty","json:target/cucumber-reports/Cucumber.json"},features="src/test/resources/Com/feature/sauce.feature",
-glue={"Stepdefenition"}
-/* ,tags =("not @Invalid-1") */
-)
-public class saucedemorunnertestng extends AbstractTestNGCucumberTests{
-	
-}
+  package runnerfile; import io.cucumber.testng.CucumberOptions; import
+  io.cucumber.testng.AbstractTestNGCucumberTests;
+  
+  @CucumberOptions(plugin={"pretty",
+  "json:target/cucumber-reports/Cucumber.json",
+  "html:target/cucumber-reports/Cucumber.html"},features=
+  "src/test/resources/Com/feature/sauce.feature",
+  glue={"Stepdefenition"},monochrome=true,dryRun=false ,tags
+  =("not @Invalid-1") ) public class saucedemorunnertestng extends
+  AbstractTestNGCucumberTests{
+  
+  }
+ 
