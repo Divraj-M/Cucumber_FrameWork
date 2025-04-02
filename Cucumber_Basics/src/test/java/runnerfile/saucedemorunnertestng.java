239bc5283e00f3d0f1630ac88b9aa8b47dbfff12
Divraj-M <2K21cse022@kiot.ac.in>
2025-04-02 10:32:17 +0530

    add json report

--- Cucumber_Basics/src/test/java/runnerfile/saucedemorunnertestng.java
+++ Cucumber_Basics/src/test/java/runnerfile/saucedemorunnertestng.java
@@ -2,7 +2,7 @@ package runnerfile;
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(plugin={"pretty","html:target/cucumber-reports/Cucumber.html"},features="src/test/resources/Com/feature/sauce.feature",
+@CucumberOptions(plugin={"pretty","json:target/cucumber-reports/Cucumber.json"},features="src/test/resources/Com/feature/sauce.feature",
 glue="Stepdefenition")
 public class saucedemorunnertestng extends AbstractTestNGCucumberTests{
 	
