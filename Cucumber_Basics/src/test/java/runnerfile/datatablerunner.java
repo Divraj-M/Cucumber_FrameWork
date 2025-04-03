192b2f436c0a396dbf4ae9ebd58874bd802d2a26
Divraj-M <2K21cse022@kiot.ac.in>
2025-04-03 12:35:03 +0530

    Update the error

--- Cucumber_Basics/src/test/java/runnerfile/datatablerunner.java
+++ Cucumber_Basics/src/test/java/runnerfile/datatablerunner.java
@@ -1,13 +1,14 @@
 package runnerfile;
 
-import io.cucumber.junit.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin={"pretty",
-		  "json:target/cucumber-reports/Cucumber.json",
-		  "html:target/cucumber-reports/Cucumber.html"},features=
-		  "src/test/resources/Com/feature/datatable.feature",
-		  glue={"Stepdefenition"})
-public class datatablerunner extends AbstractTestNGCucumberTests{
-
+@CucumberOptions(
+    plugin = {"pretty",
+              "json:target/cucumber-reports/Cucumber.json",
+              "html:target/cucumber-reports/Cucumber.html"},
+    features = "classpath:Com/feature/datatable.feature",  // Fixed feature file path
+    glue = {"Stepdefenition"}  // Ensure step definitions exist here
+)
+public class datatablerunner extends AbstractTestNGCucumberTests {
 }
